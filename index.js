fb27057244dba68935d4c305bfb00048fc4c1f3c
Nihar Reddy <niharreddy3@gmail.com>
2024-07-30 12:34:23 -0500

    added a single square to the dom

---
+++ index.js
@@ -0,0 +1,9 @@
+const divContainer = document.querySelector(".container");
+
+const squareDiv = document.createElement("div");
+
+squareDiv.style.backgroundColor = "red";
+squareDiv.style.height = "50px";
+squareDiv.style.width = "50px";
+
+divContainer.appendChild(squareDiv);
