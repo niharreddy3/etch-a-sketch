8bfc52e93c8901d7f4b2b84b1ed774ea61fb00eb
Nihar Reddy <niharreddy3@gmail.com>
2024-07-30 13:50:41 -0500

    added button logic to change the grid size, cannot exceed 100

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const divContainer = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < 16; j++) {
-    const squareDiv = document.createElement("div");
+const gridSizeButton = document.querySelector(".grid-size-button");
 
-    squareDiv.style.backgroundColor = "red";
-    squareDiv.style.height = "50px";
-    squareDiv.style.width = "50px";
-    squareDiv.classList.add("square-div");
-    squareDiv.addEventListener("mouseenter", () => {
-      squareDiv.style.backgroundColor = "blue";
-    });
+gridSizeButton.addEventListener("click", () => {
+  let sideSize = prompt("Num sq for new grid?");
 
-    row.appendChild(squareDiv);
+  // Clear previous grid if present
+  if (divContainer.hasChildNodes() && sideSize <= 100) {
+    divContainer.innerHTML = "";
   }
-  divContainer.appendChild(row);
+
+  createGrid(sideSize, divContainer);
+});
+
+function createGrid(sideSize = 16, divContainer) {
+  if (sideSize <= 100) {
+    for (let i = 0; i < sideSize; i++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      for (let j = 0; j < sideSize; j++) {
+        const squareDiv = document.createElement("div");
+
+        squareDiv.style.backgroundColor = "red";
+        squareDiv.style.height = "50px";
+        squareDiv.style.width = "50px";
+        squareDiv.classList.add("square-div");
+        squareDiv.addEventListener("mouseenter", () => {
+          squareDiv.style.backgroundColor = "blue";
+        });
+
+        row.appendChild(squareDiv);
+      }
+      divContainer.appendChild(row);
+    }
+  } else {
+    alert("Cannot exceed 100 sq on a side!");
+  }
 }
